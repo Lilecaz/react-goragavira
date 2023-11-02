2c19438a12758c975540a084d933867ea8263e56
lilecaz <lilecaz0090@gmail.com>
2023-11-02 14:14:43 +0100

    ajout d'un filtre avec le prix

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,7 +1,7 @@
 import { useContext } from 'react';
 import { CartContext } from './CardContext';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHome, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
 
 const Cart = () => {
